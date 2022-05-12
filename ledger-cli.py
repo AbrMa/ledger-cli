ad3e587668106473868d0eb18e75c53ed68ae055
Abraham Manuel <abrahammanuel@MacBook-Air-de-Abraham.local>
2022-05-12 13:05:58 -0500

    Fixed generate_report function and implemented get_money

--- ledger-cli.py
+++ ledger-cli.py
@@ -1,28 +1,47 @@
 import argparse
+from curses.ascii import isdigit
 import re
+import collections
 
-def generate_report(file_name):
-    f = open(file_name, 'r')
-    reports = {}
+def generate_report(lines):
+    report = collections.defaultdict(list)
     date = ''
     description = ''
+    sum_money = 0
 
-    for line in f.readlines():
+    for line in lines:
         line = line.partition(';')[0]
 
-        curr_date = get_str_from_regex(r'\d{4}/\d{1,2}/\d{1,2}', line)
-        if curr_date:
-            date = curr_date
-            description = line.replace(f'{curr_date} ', '').replace('\n','')
+        current_date = get_str_from_regex(r'\d{4}/\d{1,2}/\d{1,2}', line)
+        if current_date:
+            date = current_date
+            description = line.replace(f'{current_date} ', '').replace('\n','')
+            sum_money = 0
             continue
 
         account = get_str_from_regex(r'[^\d]*(\s\s|\s)', line).replace('\t','')
         money = line.replace(account, '').replace('\n', '').replace('\t', '')
 
         if account and money:
-            reports[(date, description)] = (account, money)
+            money = get_money(money)
+            sum_money += money
+            report[(date, description)].append((account, money))
+        elif account:
+            report[(date, description)].append((account, sum_money * -1))
+            
 
-    return reports
+    return report
+
+
+def get_money(str):
+    money = ''
+    for c in str:
+        if isdigit(c) or c == '.':
+            money += c
+
+    if '-' in str:
+        return float(money) * -1
+    return float(money)
 
 def get_str_from_regex(regex, str):
     str_from_regex = re.match(regex, str)
@@ -30,8 +49,15 @@ def get_str_from_regex(regex, str):
         return str_from_regex.group() 
     return ''
 
+def read_file(file_path):
+    f = open(file_path, 'r')
+    return f.readlines()
+
 def print_report(args):
-    report = generate_report(args.file)
+    lines = read_file(args.file)
+    report = generate_report(lines)
+    for val, key in report.items():
+        print(f'{val} -> {key}')
 
 def main():
     parser = argparse.ArgumentParser(description='ledger is a command-line accounting tool based on the power and completeness of double-entry accounting.  It is only a reporting tool, which means it never modifies your data files, but it does offer a large selection of reports, and different ways to customize them to your needs.')
